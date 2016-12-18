501c282e44b4ed9dd5645fc1a28c1169e3dcd93d
Mark Stacy <mbstacy@gmail.com>
2016-12-17 21:54:04 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='cybercomInstall',
+      version='1.0',
+      description='Cybercommons Installation Utility',
+      author='Mark STacy',
+      author_email='markstacy@ou.edu',
+      url='http://cybercommons.org',
+      packages=['cookiecutter',],
+     )
